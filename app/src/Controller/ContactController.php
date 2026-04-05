519cefb72e12b47d0b3e1a217073c9c7ecdadf16
davidkosvica <kosvica@1webit.cz>
2026-04-05 23:59:13 +0200

    pagination add for default listing page

--- app/src/Controller/ContactController.php
+++ app/src/Controller/ContactController.php
@@ -3,17 +3,28 @@
 namespace App\Controller;
 
 use App\Repository\ContactRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/', name: 'app_home', methods: ['GET'])]
-    public function index(ContactRepository $contactRepository): Response
-    {
+    public function index(
+        ContactRepository $contactRepository,
+        PaginatorInterface $paginator,
+        Request $request,
+    ): Response {
+        $contacts = $paginator->paginate(
+            $contactRepository->createQueryBuilder('c')->orderBy('c.surname', 'ASC'),
+            $request->query->getInt('page', 1),
+            10,
+        );
+
         return $this->render('contact/index.html.twig', [
-            'contacts' => $contactRepository->findAll(),
+            'contacts' => $contacts,
         ]);
     }
 }
