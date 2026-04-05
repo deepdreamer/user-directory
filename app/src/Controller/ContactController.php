1d9a86dccd89ffce45d5c5b45a27797ebcd9c2c5
davidkosvica <kosvica@1webit.cz>
2026-04-05 17:27:11 +0200

    default contact listing add

---
+++ app/src/Controller/ContactController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ContactRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ContactController extends AbstractController
+{
+    #[Route('/', name: 'app_home', methods: ['GET'])]
+    public function index(ContactRepository $contactRepository): Response
+    {
+        return $this->render('contact/index.html.twig', [
+            'contacts' => $contactRepository->findAll(),
+        ]);
+    }
+}
